b22a9e4a06496cf648794fc506bcbc026ae89158
Acer <Acer@DESKTOP-ABKHEEV>
2024-09-13 12:19:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+namespace ClassObject130924
+{
+    class TestPaper
+    {
+        public void sum()
+        {
+
+            int total=0;
+           
+            string str;
+            Console.WriteLine("Enter String");
+            str=Console.ReadLine();//"pravin12Gaikwad3solapur
+
+            char[] ch = str.ToCharArray();
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i]>='0'&& ch[i]<='9')
+                {
+                    // total =(int)(total + char.GetNumericValue(ch[i]));
+                    total = total + ch[i];
+                }
+            }
+            Console.WriteLine("total="+total);
+        }
+    }
+    class Class123
+    {
+        int x=100;// instance variable or field
+        //int x;
+        public void show(int a,int b)
+        {
+            int x=200; //local
+         
+
+            int y = this.x;
+            Console.WriteLine("y="+y);
+        }
+        public void show(float a, int b)
+        {
+            //return 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char cc = '1';
+            int nn = cc - '0'; 
+            Console.WriteLine("nn="+nn);
+            // Console.WriteLine(char.IsNumber(cc));
+            //Console.WriteLine(char.GetNumericValue(cc));
+
+            //student s1 = new student();
+            //s1.totalMarks = 490;
+            //s1.roll = 111;
+            //s1.showResult();
+            //Console.WriteLine("-------------------------");
+
+            //Mystud ms1=new Mystud();
+            //ms1.roll = 222;
+            //ms1.totalMarks = 490;
+            //ms1.showResult();
+
+            //new TestPaper().sum();
+
+            int[] n1 = { 1, 2, 3, 4 };
+            int [] n2 = { 3,4,5 };
+            var n3 = n1.Intersect<int>(n2).ToArray();
+
+            for (int i = 0; i < n3.Length; i++)
+            {
+                Console.WriteLine(n3[i]);
+            }
+            Console.WriteLine("-------------------------");
+            int[] n4=n1.Union<int>(n2).ToArray();
+            for (int i = 0; i < n4.Length; i++)
+            {
+                Console.WriteLine(n4[i]);
+            }
+        }
+    }
+}
---
+++ inheritanceDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject130924
+{
+    class student
+    {
+        public int roll { get; set; }
+        public int totalMarks{ get; set; }
+        protected float per;
+       virtual public void showResult()
+        {
+             per= totalMarks / 5;
+            Console.WriteLine("Roll = "+roll+" per="+per);
+        }
+    }
+    class Mystud:student
+    {
+        char grade;
+       override public void showResult()//extensiblity
+        {
+            base.showResult();//reuse
+
+            if (per>90)
+            {
+                grade = 'A';
+            }
+            else
+            {
+                grade = 'B';
+            }
+            Console.WriteLine("Grade ="+grade);
+        }
+    }
+    internal class inheritanceDemo
+    {
+    }
+}
